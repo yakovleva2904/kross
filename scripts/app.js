1ed596555c95382ceddd48419fe83db32399df12
antipenko <sweetglory.victory@gmail.com>
2019-07-30 17:42:10 +0300

    added slider in js file

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $('.ba-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll:2,
+        arrows: false,
+        infinite: false,
+        dots: true,
+        autoPlay: true,
+        appendDots: '.slider-nav'
+    });
+
+});
+
