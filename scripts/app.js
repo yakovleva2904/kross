963c4b0a653d747018edb888178016c9eeacf79b
antipenko <sweetglory.victory@gmail.com>
2019-07-30 22:52:35 +0300

    add google map

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+
+    // slider 
     $('.ba-slider').slick({
         slidesToShow: 4,
         slidesToScroll:2,
@@ -11,4 +13,29 @@ $(document).ready(function() {
     });
 
 });
+
+// include google maps
+const googleMapsScript = document.createElement('script');
+	googleMapsScript.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyBaSe6jdgxnsPBSc7pRB0_MlIoZSRm7aw8&callback=initMap';
+    document.head.appendChild(googleMapsScript);
+    
+// init google map
+function initMap() {
+
+    var markerBA = {lat: 50.006693, lng: 36.237199};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: markerBA,
+        zoom: 19
+    });
+
+    var marker = new google.maps.Marker({
+        position: baOffices[i].position,
+        title: 'ba-map title',
+        // icon: 'path/to/icon.png',
+        map: map
+      });
+}
+
+
 
